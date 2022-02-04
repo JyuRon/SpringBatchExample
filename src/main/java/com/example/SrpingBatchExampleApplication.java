071a90b51c5193413278f2d130a41a14e5de028e
JyuKa <yoyksks3@gmail.com>
2022-02-04 12:53:56 +0900

    Initial commit

---
+++ src/main/java/com/example/SrpingBatchExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpingBatchExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrpingBatchExampleApplication.class, args);
+	}
+
+}
