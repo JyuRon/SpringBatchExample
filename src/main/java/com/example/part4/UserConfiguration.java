b1b5418f4323cebd351bd92b84ec47efbeb26b2f
JyuKa <yoyksks3@gmail.com>
2022-02-04 19:25:55 +0900

    [회원등급프로젝트] init된 User의 회원등급 보정 및 test code

--- src/main/java/com/example/part4/UserConfiguration.java
+++ src/main/java/com/example/part4/UserConfiguration.java
@@ -6,6 +6,11 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,7 +22,7 @@ import javax.sql.DataSource;
 public class UserConfiguration {
 
     private final String JOB_NAME = "userJob";
-    private final int CHUNK = 1000;
+    private final int CHUNK = 100;
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
     private final UserRepository userRepository;
@@ -42,17 +47,64 @@ public class UserConfiguration {
             return this.jobBuilderFactory.get(JOB_NAME)
                     .incrementer(new RunIdIncrementer())
                     .start(this.saveUserStep())
+                    .next(this.userLevelUpStep())
                     .build();
     }
 
 
 
+    // init user
     @Bean(JOB_NAME + "_saveUserStep")
     public Step saveUserStep() {
         return this.stepBuilderFactory.get(JOB_NAME + "_saveUserStep")
                 .tasklet(new SaveUserTasklet(userRepository))
                 .build();
     }
+
+    // init user된 회원의 경우 등급이 모두 normal이기 때문에 등급 보정이 필요
+    @Bean(JOB_NAME + "_userLevelUpStep")
+    public Step userLevelUpStep() throws Exception {
+        return this.stepBuilderFactory.get(JOB_NAME + "_userLevelUpStep")
+                .<User,User>chunk(CHUNK)
+                .reader(itemReader())
+                .processor(itemProcessor())
+                .writer(itemWriter())
+                .build();
+    }
+
+    private ItemReader<? extends User> itemReader() throws Exception {
+        JpaPagingItemReader<User> itemReader = new JpaPagingItemReaderBuilder<User>()
+                .queryString("select u from User u")
+                .entityManagerFactory(entityManagerFactory)
+                .pageSize(CHUNK)
+                .name(JOB_NAME + "_userItemReader")
+                .build();
+
+        itemReader.afterPropertiesSet();
+        return itemReader;
+
+    }
+
+    private ItemProcessor<? super User,? extends User> itemProcessor() {
+        return user->{
+            if(user.avaliableLevelUp()){
+                return user;
+            }
+            return null;
+        };
+    }
+
+
+    private ItemWriter<? super User> itemWriter() {
+        return users-> users.forEach(x->{
+            x.levelUp();
+            userRepository.save(x);
+        });
+    }
+
+
+
+
 
 
 }
